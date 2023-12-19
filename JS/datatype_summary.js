b66d9443257446bbde64f003b51b405d02d44a4c
Damn-Nachiket <142562120+Damn-Nachiket@users.noreply.github.com>
2023-12-19 16:15:22 +0000

    Created datatype_summary.js File

---
+++ JS/datatype_summary.js
@@ -0,0 +1,31 @@
+/* Premititve
+   7 Premitive types  : string | Number | Boolean |     null | undefined | Symbol | BigInt .
+*/
+
+const score = 100 //number
+const scoreValue = 98.7
+
+const isLoggedIn = false //boolean
+
+const tempratue = null //null
+
+const user_mail = undefined // let user-mail;
+
+const u_id = Symbol('154') // Symbol
+const anotherId = Symbol('154')
+
+const bigNumber = 6785647n //BigInt
+
+/* Reference (Non - Premitive)
+   Types : Array | Objects (Master This) | Browser web events (Master This) | Function.
+*/
+
+const stocks = ["TCS" , "Adani Gas" , "Infosys"];//Array
+let obj= {
+    name : "Ratan",
+    age : 65,
+} // The values in Curly Braces are Object
+
+const func = function(){
+    console.log("Hellow World");
+} // One method : Creating function inside Variable
