17b635a6168f0241f37c1aa1682b08bad1337417
Damn-Nachiket <142562120+Damn-Nachiket@users.noreply.github.com>
2023-12-26 15:21:18 +0000

    Created nums and math js file

---
+++ JS/06_nums_and _math.js
@@ -0,0 +1,12 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(200) //Specially provide Number datatype to value
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.98665 //123.8966
+
+console.log(otherNumber.toPrecision());
