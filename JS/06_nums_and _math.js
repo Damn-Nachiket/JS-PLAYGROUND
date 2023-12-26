85f3ec557983758efb5bce5eca4d8eb41a1a9852
Damn-Nachiket <142562120+Damn-Nachiket@users.noreply.github.com>
2023-12-26 17:33:22 +0000

    Modified Sums_and_maths.js

--- JS/06_nums_and _math.js
+++ JS/06_nums_and _math.js
@@ -10,3 +10,26 @@ console.log(balance.toFixed(2));
 const otherNumber = 23.98665 //123.8966
 
 console.log(otherNumber.toPrecision());
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// In console write : Number.(all function will appear) Search them anf learn them for DSA
+
+//__________________Maths________________________
+
+console.log(Math); // Write In console
+console.log(Math.abs(-4));//Negative to positive
+console.log(Math.round(4.3));// Roundof Value
+console.log(Math.ceil(4.2));//Output : 5
+console.log(Math.floor(4.2)); //Output : 4
+console.log(Math.min(4,3,5,6,1)); //Find the min value from array
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
