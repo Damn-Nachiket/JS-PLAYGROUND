f21a6fda5c76014abaec624c875eba1b24d469b7
Nachiket Patel <142562120+Damn-Nachiket@users.noreply.github.com>
2023-12-28 17:31:04 +0000

    Understand Array in JS

---
+++ JS_Basic_02/01_array.js
@@ -0,0 +1,31 @@
+//array
+
+const myArr = [0,1,3,4,5]
+const myHeros = ["BMW" , "Audi"]
+
+const myArr2 = new Array(1,2,3,4)
+
+console.log(myArr[0]);
+
+//Array Methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(0)
+// myArr.shift()
+// myArr.includes(9)
+//myArr.indexOf(13)
+
+const new_arr = myArr.join
+console.log(new_arr);
+console.log(myArr);
+
+//slice , splice
+
+console.log("A" , myArr)
+const myN1 = myArr.slice(1,3)
+console.log(myN1);
+console.log("B ",myArr);
+const myN2 = myArr.splice(1,3)
+console.log("C ",myArr);
+console.log(myN2);
